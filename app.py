15d6a20ca6a03ab45ba6429dc33cb21411403a1e
Anangsha072 <146315632+Anangsha072@users.noreply.github.com>
2024-07-08 10:22:40 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -173,14 +173,12 @@ def search():
 
     return jsonify(results)
 
-
-
-
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
         create_sample_data()  # Optional: Populate with sample data
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
 
 
 
